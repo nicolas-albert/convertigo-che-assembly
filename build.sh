929bc6ffee8a803579e2953471242e9addcae632
Nicolas Albert <nicolas.albert@gmail.com>
2017-07-10 11:14:26 +0200

    chown

--- build.sh
+++ build.sh
@@ -15,16 +15,7 @@ init "$@"
 
 DEFAULT_MVN_REPO=${HOME}/.m2/repository
 
-if command -v mvn >/dev/null 2>&1; then
-  MVN_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -E '^([a-zA-Z]:|/)')
-  echo "Detected Maven, mounting local repo at ${MVN_REPO}"
-else
-  MVN_REPO=${DEFAULT_MVN_REPO}
-  echo "Maven not detected, mounting local repo at default path: ${DEFAULT_MVN_REPO}";
-fi
-
-#mkdir -p $MVN_REPO
-#sudo chown -R $USER $MVN_REPO
+sudo chown -R $USER ${HOME}/.m2
 echo "ls -la"
 ls -la
 echo "id"
